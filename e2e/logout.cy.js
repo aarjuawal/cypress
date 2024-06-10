906cf25bfc5c4a083c1f655fb7786c73ca8ffbc8
aarju awal <aarzuawal@gmail.com>
2024-06-10 15:46:20 +0545

    update

--- e2e/logout.cy.js
+++ e2e/logout.cy.js
@@ -4,6 +4,16 @@ describe("logout sucessfully",()=>{
     it("logout",()=>{
         cy.visit("https://roles.staging.rigohr.com/login")
         cy.get(".css-1gbfbiu").click()
-        cy.contails("Sign Out").click
+        cy.contains("Sign Out").click()
     })
+})
+
+//to check the allowance request with valid and invalid credentials
+
+describe("allowance request ",()=>{
+    it("allowance request with valid credentials",()=>{
+    cy.visit("")
+    cy.get().click()
+    cy.contains("Allowance Request").click()
+})
 })
